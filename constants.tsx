db7b2941b2244cf880c24c49ee80d64a72bcd883
Arlita <arlitacantika99@gmail.com>
2026-07-06 00:46:08 +0700

    aturan transaksi

--- constants.tsx
+++ constants.tsx
@@ -1,7 +1,7 @@
 
 import { MenuItem, Voucher } from './types';
 
-const CENTRAL_BACKEND_URL = 'http://10.20.113.142:4000';
+const CENTRAL_BACKEND_URL = 'http://localhost:4000';
 
 const getEnv = (key: string) =>
   (import.meta as any).env?.[key] || (globalThis as any).process?.env?.[key];
